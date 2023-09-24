a8a9c6eb50be4beb10aecd285bc18d228def2e4c
Leo <ribotleo@gmail.com>
2023-09-24 09:39:34 +0200

    fix function name fetchCars

--- utils/index.ts
+++ utils/index.ts
@@ -1,9 +1,9 @@
-export async function fecthCars() {
+export async function fetchCars() {
     const headers = {
         'X-RapidAPI-Key': '36d65eee45mshf476a00f24c339bp1bcb26jsn4d39e726a5ad',
         'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
     }
-    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars', {headers: headers});
+    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {headers: headers});
 
     return await response.json();
 }
