3d09c7bb5ff6bdc387f52feb6a58b871f5402ac4
Leo <ribotleo@gmail.com>
2023-09-24 21:26:31 +0200

    feat(searchbar) add filter

--- utils/index.ts
+++ utils/index.ts
@@ -1,11 +1,12 @@
-import {CarProps} from "@/types";
+import {CarProps, FilterProps} from "@/types";
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
+    const {manufacturer, year, model, limit, fuel} = filters;
     const headers = {
         'X-RapidAPI-Key': '36d65eee45mshf476a00f24c339bp1bcb26jsn4d39e726a5ad',
         'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
     }
-    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3', {headers: headers});
+    const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`, {headers: headers});
 
     return await response.json();
 }
@@ -38,4 +39,9 @@ export const generateCarImageUrl = (car: CarProps, angle?: String) => {
     url.searchParams.append('angle',`${angle}`);
 
     return `${url}`;
+}
+export const updateSearchParams = (type: string, value: string) => {
+    const searchParams = new URLSearchParams(window.location.search);
+    searchParams.set(type, value);
+    return `${window.location.pathname}?${searchParams.toString()}`;
 }
